f1009d6bf0abf99854bfc560ce19aa87cdf59f35
ahmedaryf <ahmed.areef@hotmail.com>
2023-03-21 09:59:12 +0500

    initial commit

---
+++ src/DessertsList.js
@@ -0,0 +1,21 @@
+
+function DessertsList(props) {
+    // Implement the component here.
+    
+    const dessertList = props.data.filter(item => item.calories < 500).sort((a,b) => a.calories - b.calories).map(dessert => (
+        <li key={dessert.name}>
+          {dessert.name} {dessert.calories} cal
+        </li>
+      ))
+    
+    return (
+        <div>
+          <ul>
+            {dessertList}
+          </ul>
+        </div>
+      );
+  }
+  
+  export default DessertsList;
+  
